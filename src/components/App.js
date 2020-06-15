4bd99a46f63a9a151169e5b8f936be019eb6be4d
Darshan C <darshansagar.chudiwal2018@vitstudent.ac.in>
2020-06-15 23:09:27 +0530

    final and the only commit!

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,89 @@
 import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
 
-const Input = styled.input`
-  border : none;
-  border-bottom : 1px solid grey;
-`
-const Button = styled.button`
-  border : 0px;
-  border-radius : 3px;
-  background : yellow;
-`
 function App() {
-  //add a task
-  const [task, setTask] = useState([]);
-  const [taskElement, setTaskElement] = useState({
-    text: '',
-    key: 0,
-  })
-  const [val, setVal] = useState('');
-  //making the form controlled
-  function handleChange(event) {
-    const { value } = event.target;
-    console.log(value)
+  const [todos, setTodos] = useState([
+    {
+      content: 'Pickup dry cleaning',
+      isCompleted: true,
+    },
+    {
+      content: 'Get haircut',
+      isCompleted: false,
+    },
+    {
+      content: 'Build a todo app in React',
+      isCompleted: false,
+    }
+  ]);
+
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
   }
 
-  //add task
-  function addTask() {
-    console.log('task added')
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function toggleTodoCompleteAtIndex(index) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+    setTodos(temporaryTodos);
   }
 
   return (
-    <div>
-      <form onSubmit={addTask}>
-        <Input type="text" value={taskElement} onChange={handleChange} />
-        <Button />
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+            <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
+              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
+                {todo.isCompleted && (
+                  <span>&#x2714;</span>
+                )}
+              </div>
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)}
+                onChange={e => updateTodoAtIndex(e, i)}
+              />
+            </div>
+          ))}
+        </ul>
       </form>
-
     </div>
   );
 }
